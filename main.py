0bf96fcd063fcce976dbfc018fd69f3d114e334b
nanezzio <kubik.moyo06@gmail.com>
2026-02-28 00:54:41 +0300

    create class task

---
+++ main.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+class Task:
+    def __init__(self, name, deadline):
+        self.name = name
+        self.dateCreate = datetime.now()
+        self.passed = False 
+        self.deadline = deadline
+
+    def update(self, name, deadline):
+        self.name = name
+        self.deadline = deadline
+
+    def change_pass(self):
+        self.passed = not self.passed
+
+    def set_pass(self, passed):
+        self.passed = passed
+
+    def get_name(self):
+        return self.name
+
+    def get_dateCreate(self):
+        return self.dateCreate
+
+    def get_passed(self):
+        return self.passed
+
+    def get_deadline(self):
+        return self.deadline
+    
+
+
+
+
+
+
+
+
+    
+
+
